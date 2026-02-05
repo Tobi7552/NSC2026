fd28971a88ab239207909375709076f07dbac9fa
Tobi7552 <60338540+Tobi7552@users.noreply.github.com>
2026-02-05 13:44:59 +0100

    First commit
    Added the yml file

---
+++ Kursusgang 1/test_install.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib, scipy, numba, pytest, dask, distributed
+
+print(np.__version__)
+print(matplotlib.__version__)
+print(scipy.__version__)
+print(numba.__version__)
+print(pytest.__version__)
+print(dask.__version__)
+print(distributed.__version__)

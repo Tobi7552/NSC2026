afb4f2bfd11ca5a5ff5487e00b968c85343c86b5
Tobi7552 <60338540+Tobi7552@users.noreply.github.com>
2026-02-19 12:38:57 +0100

    Update mandelbrot.py

--- mandelbrot-nsc/mandelbrot.py
+++ mandelbrot-nsc/mandelbrot.py
@@ -8,15 +8,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
-def mandelbrot_point(x_min:float, x_max:float, y_min:float, y_max:float, max_iter:int, number:int):
-    z = 0
-    xDomain, yDomain = np.linspace(x_min, x_max,1000), np.linspace(y_min, y_max,1000)
-    
-
-
-    c = complex(x,y)
-    pass
-
 
 def compute_mandelbrot():
     start = time.time()
